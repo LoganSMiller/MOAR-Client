b9aecd3f6256dcde0338d576a7131788a8b02c8f
Logan Miller <110432344+LoganSMiller@users.noreply.github.com>
2025-04-08 16:58:26 -0400

    enum util push

---
+++ Helpers/EnumUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MOAR.Helpers
+{
+    public static class EnumUtils
+    {
+        public static T SafeParseEnum<T>(string value, T fallback) where T : struct, Enum
+        {
+            if (string.IsNullOrEmpty(value))
+                return fallback;
+
+            return Enum.TryParse<T>(value, ignoreCase: true, out var result) ? result : fallback;
+        }
+    }
+}
